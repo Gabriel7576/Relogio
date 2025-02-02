43c702a7808c5f01d10d8cc94eb4114926ddf874
unknown <gabrielamaral7575@gmail.com>
2025-02-02 00:20:46 -0300

    First release of Hello World!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'Relogio';
+export class AppComponent implements OnInit{
+
+  constructor(){}
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.changeText('h1',this.changeTime());
+    }, 1000);
+  }
+
+  changeTime():string{
+    let hours = new Date().getHours();
+    let minutes = new Date().getMinutes();
+    let seconds = new Date().getSeconds();
+
+    return `${this.addZero(hours)}:${this.addZero(minutes)}:${this.addZero(seconds)}`;
+  }
+
+  addZero(time:number){
+    return time.toString().padStart(2,'0');
+  }
+
+  changeText(element:any,text:any) {
+    let el = document.querySelector(element);
+    el.innerHTML = text;
+  }
+
 }
