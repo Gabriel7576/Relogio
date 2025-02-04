295404053b8d5ba35f2de319e7967c15469c0333
Gabriel7576 <gabrielamaral7575@gmail.com>
2025-02-04 16:37:07 -0300

    Alterção do texto principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,7 @@ export class AppComponent implements OnInit{
     let minutes = new Date().getMinutes();
     let seconds = new Date().getSeconds();
 
-    return `${this.addZero(hours)}:${this.addZero(minutes)}:${this.addZero(seconds)}`;
+    return `T: ${this.addZero(hours)}:${this.addZero(minutes)}:${this.addZero(seconds)}`;
   }
 
   addZero(time:number){
